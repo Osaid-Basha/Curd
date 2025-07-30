0a38d520cc833004482b8b07b2c9cbe431d39594
Osaid-Basha <albashaosayd@gmail.com>
2025-07-30 15:23:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Create from './pages/user/Create';
+import About from './pages/about/About';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Home />} />
+        <Route path="/add" element={<Create />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
